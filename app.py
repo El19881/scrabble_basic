212a666205dc59db4f706ebf7f4d69f3816b5b0a
Ariel Ferensztajn <77733595+El19881@users.noreply.github.com>
2022-01-09 19:33:56 +0100

    Add files via upload
    This version has the basic functionalities of:
    - adding new players
    - recognizing how many players there are
    - adding points per round to the players
    - adding up all points and showing a Game Results page when finishing the game
    - some basic styling/CSS functionalities for buttons etc.

--- app.py
+++ app.py
@@ -1,31 +1,37 @@
+#create a new py file with routes and a file for just the flask app and then import it to the route files
+
 from flask import Flask, render_template, request
-from dictionaries import players
+from player_lists import player1, player2, player3, player4, roundssss, list_scores
+from def_add_new_players import add_new_players
+from def_add_points import add_points
+from def_final_results import final_results
 
 app = Flask(__name__)
+
 
 @app.route('/', methods = ["GET", "POST"])
 def index():
   ''' Landing page. Players submit their names '''
+  return render_template("add_players.html", title = "Add players")
 
-  return render_template("add_players.html", title = "Dodaj graczy")
 
 @app.route("/points", methods = ["GET", "POST"])
 def points():
   ''' List of players, their points and forms to add new points and end the game '''
+  add_new_players()
+  add_points()
+  
+  return render_template("points.html", title = "Players and points", player1=player1, player2=player2, player3=player3, player4=player4)
 
-  player_one = request.form["player_input_1"]
-  if player_one:
-    players[player_one] = 0
-  player_two = request.form["player_input_2"]
-  if player_two:
-    players[player_two] = 0
-  player_three = request.form["player_input_3"]
-  if player_three:
-    players[player_three] = 0
-  player_four = request.form["player_input_4"]
-  if player_four:
-    players[player_four] = 0
-  return render_template("points.html", title = "Gracze i punkty", player_dict = players, player_one = player_one, player_two = player_two, player_three = player_three, player_four = player_four)
+
+@app.route("/end_game", methods = ["POST", "GET"])
+def end_game():
+  ''' Final page of the app, shows the results of the game '''
+  final_results()
+  rounds = len(roundssss)
+
+  return render_template ("end_game.html", title = "Final results", player1=player1, player2=player2, player3=player3, player4=player4, list_scores=list_scores, rounds = rounds)
+
 
 app.run(debug=True)
 
